90be9c086384b40f8990a0bfd50394420e54ff8f
oht <duduio2050@gmail.com>
2024-07-24 13:58:24 +0900

    level1 - 6 코드 수정

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -20,18 +20,27 @@ public class App {
             System.out.print("두 번째 숫자를 입력하세요: ");
             // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
             int secondNum = sc.nextInt();
-            if(secondNum == 0){
-                System.out.print("두번째 정수는 0 이 될 수 없습니다.");
-                // 입력창 닫기
-                sc.close();
-                // 메서드 종료
-                return;
+
+            while(secondNum == 0) {
+                System.out.print("두번째 정수는 0 이 될 수 없습니다. 다시 입력해주세요 : ");
+                // 0 인 경우 입력값 다시받기
+                secondNum = sc.nextInt();
+//                // 입력창 닫기
+//                sc.close();
+//                // 메서드 종료
+//                return;
             }
 
             System.out.print("사칙연산 기호를 입력하세요: ");
             // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
             String inputSymbols = sc.next();
             char symbols = inputSymbols.charAt(0);
+
+            while(symbols != '+' && symbols != '-' && symbols != '*' && symbols != '/'){
+                System.out.print("올바른 연산 기호를 입력해주세요. 다시 입력해주세요 : ");
+                inputSymbols = sc.next();
+                symbols = inputSymbols.charAt(0);
+            }
 
             int total = 0;
             switch(symbols){
@@ -47,10 +56,6 @@ public class App {
                 case '/':
                     total = firstNum / secondNum;
                     break;
-                default:
-                    System.out.println("올바른 연산 기호를 입력하세요.");
-                    sc.close();
-                    return;
             }
 
             if(count == 10){
